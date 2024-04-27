452da6f860101b8508c0e24781c648fb16a9426f
hukabo <dlwlsdn153@gmail.com>
2024-04-27 23:55:43 +0900

    feat: handleMethodArgumentNotValidException() 추가

--- subject/src/main/java/com/example/subject/exception/GlobalExceptionAdvice.java
+++ subject/src/main/java/com/example/subject/exception/GlobalExceptionAdvice.java
@@ -3,6 +3,7 @@ package com.example.subject.exception;
 import com.example.subject.response.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -14,14 +15,24 @@ public class GlobalExceptionAdvice {
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
     public ErrorResponse handleBusinessLogicException(BusinessLogicException e) {
+        log.error("BusinessLogicException: ", e);
 
         return ErrorResponse.of(e.getExceptionCode());
     }
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorResponse handleException(Exception e) {
         log.error("Exception: ", e);
 
         return ErrorResponse.of(e);
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        log.error("MethodArgumentNotValidException: ", e);
+
+        return ErrorResponse.of(e.getBindingResult());
     }
 }
