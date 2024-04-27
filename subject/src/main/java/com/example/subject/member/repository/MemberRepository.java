39dab86d1de8dd664ce609bd1f8cca053d6927da
hukabo <dlwlsdn153@gmail.com>
2024-04-27 14:31:00 +0900

    chore: memberRepositoryImple -> memberRepository(JpaRepository)

--- subject/src/main/java/com/example/subject/member/repository/MemberRepository.java
+++ subject/src/main/java/com/example/subject/member/repository/MemberRepository.java
@@ -7,10 +7,7 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberRepository {
-
-    Member save(Member member);
-    Optional<Member> findById(Long memberId);
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long>{
     Optional<Member> findByEmail(String email);
-    List<Member> findAll();
 }
