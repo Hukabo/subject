27d4c5854d19d3e7f25c514b10b98bee8ba3ad37
hukabo <dlwlsdn153@gmail.com>
2024-04-27 12:31:02 +0900

    feat: 메서드 실행시간 측정 AOP

---
+++ subject/src/main/java/com/example/subject/aop/TimeTraceAop.java
@@ -0,0 +1,24 @@
+package com.example.subject.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class TimeTraceAop {
+
+    @Around("execution(* com.example.subject..*(..))")
+    public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
+        long start = System.currentTimeMillis();
+        System.out.println("START: " + joinPoint.toString());
+        try {
+            return joinPoint.proceed();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long result = finish - start;
+            System.out.println("END: " + joinPoint.toString() + " " + result + "ms");
+        }
+    }
+}
