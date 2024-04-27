f214f1928eeceaec90c396cddd99b3321843947c
hukabo <dlwlsdn153@gmail.com>
2024-04-27 14:31:48 +0900

    feat: findMemberList() 추가

--- subject/src/main/java/com/example/subject/member/service/MemberService.java
+++ subject/src/main/java/com/example/subject/member/service/MemberService.java
@@ -1,18 +1,19 @@
 package com.example.subject.member.service;
 
 import com.example.subject.member.entity.Member;
-import com.example.subject.member.exception.BusinessLogicException;
-import com.example.subject.member.exception.ExceptionCode;
+import com.example.subject.exception.BusinessLogicException;
+import com.example.subject.exception.ExceptionCode;
 import com.example.subject.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
 import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
@@ -22,6 +23,15 @@ public class MemberService {
         verifyExistMember(member.getEmail()); // 회원 가입 시, 이미 존재하는 회원인지 검증
 
         return memberRepository.save(member);
+    }
+
+    public Page<Member> findMemberList(int page, int pageSize, String field, String direction) {
+
+        if (direction.equals("desc")) {
+            return memberRepository.findAll(PageRequest.of(page, pageSize).withSort(Sort.by(Sort.Direction.DESC, field)));
+        } else {
+            return memberRepository.findAll(PageRequest.of(page, pageSize).withSort(Sort.by(Sort.Direction.ASC, field)));
+        }
     }
 
     public void verifyExistMember(String email) {
