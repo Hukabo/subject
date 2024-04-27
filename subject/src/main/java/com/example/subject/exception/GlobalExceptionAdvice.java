7d101f1993e5f23c49103120445de867fcbcf600
hukabo <dlwlsdn153@gmail.com>
2024-04-27 14:25:59 +0900

    feat: handleException 메서드 추가

--- subject/src/main/java/com/example/subject/exception/GlobalExceptionAdvice.java
+++ subject/src/main/java/com/example/subject/exception/GlobalExceptionAdvice.java
@@ -1,15 +1,28 @@
-package com.example.subject.member.exception;
+package com.example.subject.exception;
 
-import com.example.subject.member.errorResponse.ErrorResponse;
+import com.example.subject.response.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.exception.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionAdvice {
 
     @ExceptionHandler
+    @ResponseStatus(HttpStatus.NOT_ACCEPTABLE)
     public ErrorResponse handleBusinessLogicException(BusinessLogicException e) {
 
         return ErrorResponse.of(e.getExceptionCode());
+    }
+
+    @ExceptionHandler
+    public ErrorResponse handleException(Exception e) {
+        log.error("Exception: ", e);
+
+        return ErrorResponse.of(e);
     }
 }
