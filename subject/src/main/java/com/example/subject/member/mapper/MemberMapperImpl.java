e7130cc084c37694da3e945659ab11fea1c3c93e
hukabo <dlwlsdn153@gmail.com>
2024-04-27 16:17:34 +0900

    feat: patchMemberDtoToMember() 추가

--- subject/src/main/java/com/example/subject/member/mapper/MemberMapperImpl.java
+++ subject/src/main/java/com/example/subject/member/mapper/MemberMapperImpl.java
@@ -4,10 +4,12 @@ import com.example.subject.member.dto.MemberDto;
 import com.example.subject.member.entity.Member;
 import org.springframework.stereotype.Component;
 
+import static com.example.subject.member.dto.MemberDto.*;
+
 @Component
 public class MemberMapperImpl implements MemberMapper {
 
-    public Member postMemberDtoToMember(MemberDto.PostMemberDto postMemberDto) {
+    public Member postMemberDtoToMember(PostMemberDto postMemberDto) {
 
         if (postMemberDto == null) {
             return null;
@@ -19,6 +21,20 @@ public class MemberMapperImpl implements MemberMapper {
                 .name(postMemberDto.getName())
                 .nickName(postMemberDto.getNickName())
                 .phoneNumber(postMemberDto.getPhoneNumber())
+                .build();
+    }
+
+    public Member patchMemberToMember(PatchMemberDto patchMemberDto) {
+
+        if (patchMemberDto == null) {
+            return null;
+        }
+
+        return Member.builder()
+                .password(patchMemberDto.getPassword())
+                .name(patchMemberDto.getName())
+                .nickName(patchMemberDto.getNickName())
+                .phoneNumber(patchMemberDto.getPhoneNumber())
                 .build();
     }
 }
