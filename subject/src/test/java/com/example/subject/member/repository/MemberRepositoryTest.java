b821c8e5f8b0951ae6f85447527be1268cfc414d
hukabo <dlwlsdn153@gmail.com>
2024-04-28 14:23:44 +0900

    chore: @DataJpaTest로 변경

--- subject/src/test/java/com/example/subject/member/repository/MemberRepositoryTest.java
+++ subject/src/test/java/com/example/subject/member/repository/MemberRepositoryTest.java
@@ -5,14 +5,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
 
-@SpringBootTest
-@Transactional
+@DataJpaTest
 class MemberRepositoryTest {
     @Autowired
     MemberRepository memberRepository;
